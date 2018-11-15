89cae77c2f4dafadbd70622562593215dfe1dcd6
Woo Joo Yeon <yeon.woojoo@nbt.com>
2018-11-15 16:19:55 +0900

    Updates

---
+++ precache-manifest.b90c5bcb28e0221bca1fb7a9d1b36b58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "37a4cff37257b273a531",
+    "url": "/movie_app/static/js/main.37a4cff3.chunk.js"
+  },
+  {
+    "revision": "0b41945e2db551bfebda",
+    "url": "/movie_app/static/js/1.0b41945e.chunk.js"
+  },
+  {
+    "revision": "37a4cff37257b273a531",
+    "url": "/movie_app/static/css/main.4009526f.chunk.css"
+  },
+  {
+    "revision": "cdbbbfd89364bd53245be8deaf63222a",
+    "url": "/movie_app/index.html"
+  }
+];
